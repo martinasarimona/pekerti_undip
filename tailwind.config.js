09c8cc1037494a6968a7e4136ab020d0de0e6ea0
martinasarimona <martinasarimona@gmail.com>
2025-09-01 14:06:53 +0700

    feat: port demo HTML to landing blade view

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,21 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                navy: {
+                    50: '#f0f4ff',
+                    100: '#e0e9ff',
+                    200: '#c7d6fe',
+                    300: '#a5b8fc',
+                    400: '#8b93f8',
+                    500: '#7c6df2',
+                    600: '#6d4de6',
+                    700: '#5d3dcb',
+                    800: '#4c32a4',
+                    900: '#1e3a8a',
+                    950: '#0f172a',
+                },
+            },
         },
     },
 
